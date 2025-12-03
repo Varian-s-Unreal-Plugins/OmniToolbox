43900429d1ada43dc871f63021b99a17304f9038
Variann <kjartandth@gmail.com>
2025-12-03 03:01:44 +0100

    Fix: documentation button crashes when opening corrupted assets

--- Source/OmniToolbox/Private/Developer/I_AssetDetails.cpp
+++ Source/OmniToolbox/Private/Developer/I_AssetDetails.cpp
@@ -50,6 +50,12 @@ Omni_OnModuleStarted("OmniToolboxEditor")
 						{
 							TargetObject = EditedAsset;
 						}
+						
+						if(TargetObject == nullptr)
+						{
+							/**Asset might be broken if we hit this*/
+							break;
+						}
 
 						if(!TargetObject->Implements<UI_AssetDetails>())
 						{
